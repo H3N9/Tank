3746835696f72006d6a54d9e711a92a939477c04
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2019-11-24 14:49:21 +0700

    fix

--- Big-Gun/src/menu/prePlay.java
+++ Big-Gun/src/menu/prePlay.java
@@ -1760,7 +1760,7 @@ public class prePlay extends javax.swing.JFrame{
         jLabel152.setBounds(0, 60, 70, 20);
 
         tiger2_p.setFont(new java.awt.Font("Impact", 0, 15)); // NOI18N
-        tiger2_p.setText(""+(int)tanks.tanks.get("tiger")[6]);
+        tiger2_p.setText(""+(int)tanks.tanks.get("tiger2")[6]);
         jPanel86.add(tiger2_p);
         tiger2_p.setBounds(60, 60, 70, 20);
 
